cee2a420a69d6590e6751b62893cbf5b5fd4df50
ryliezachary <losganoryan0808@gmail.com>
2019-08-18 01:03:59 +0800

    Finish Slide

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
-	$('#slides').superslides();
+	$('#slides').superslides({
+		animation: "fade",
+		play: 8000,
+		pagination: false
+	});
+
+	var typed=new Typed(".typed", {strings:["WEB DEVELOPER","VIRTUAL ASSISTANT","SHOPIFY/WOOCOMMERCE SPECIALIST"],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor:false
 
 });
+
+}); 
