f00248536ad58fe0aa5b5e14e6c9f103971f1960
ryliezachary <losganoryan0808@gmail.com>
2019-08-18 23:48:39 +0800

    About and Skills Section

--- js/script.js
+++ js/script.js
@@ -14,4 +14,43 @@ $(document).ready(function(){
 
 });
 
+$('.owl-carousel').owlCarousel({
+    loop:true, 
+    items: 4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+            items:4
+        }
+    }
+});
+
+   
+   var skillsTopOffset=$(".skillSection").offset().top;
+
+  	$(window).scroll(function(){
+
+  		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+  			$('.chart').easyPieChart({
+   	easing: 'easeInOut',
+   	barColor: '#f2f6f5',
+   	trackColor: false,
+   	scaleColor: false,
+   	lineWidth: 4,
+   	size: 152,
+   	onStep: function(from,to,percent){
+   		$(this.el).find('.percent').text(Math.round(percent));
+   	}
+   });
+			
+  		}
+  	});
 }); 
